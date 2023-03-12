9bd869fac4465db960f7272f344446cfd4bce772
Aruan-creator <aruan.kot3@gmail.com>
2023-03-12 14:16:13 +0600

    change init to prepare args

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ def sqrt(D):
 
 def discrim(a, b, c):
     D = (b**2) - (4 * a * c)
+    print(D)
     return D
 
 def two_roots(a, b, D):
     x1 = (-b + sqrt(D)) / (2 * a)
     x2 = (-b - sqrt(D)) / (2 * a)
-    return [x1, x2]
+    return [ x1, x2 ]
 
 def one_root(b, a):
     result = -b / 2*a
@@ -16,8 +17,6 @@ def one_root(b, a):
 
 def get_roots(a, b, c):
     print(a, b, c)
-    #result = 0
-    discrim(a, b, c)
     if(discrim(a, b, c) > 0):
         return two_roots(a, b, c)
     elif(discrim(a, b, c)== 0):
@@ -25,30 +24,17 @@ def get_roots(a, b, c):
     elif(discrim(a, b, c) < 0):
         return False
 
-def init(*args):
-    print(args)
-    if(len(args) != 3):
-        return False
-    else:
-        return True
-
-def listToInt(args):
-    result = []
-    for i in args:
-        result.append(int(i))
-        print(result)
-    return result
+def prepareArg(arg):
+    #add some new predicates
+    return int(arg)
 
 def main():
-    #2, 4, 12
-    while(True):
-        args = input().split()
-        intArgs = listToInt(args)
-        if init(intArgs[0]) == False:
-            continue
-        else:
-            break
-    result = init(intArgs[0])
+    #2, 4, 12 = False
+    #2, 5, -3 = -3, 1/2
+    a = prepareArg(input())
+    b = prepareArg(input())
+    c = prepareArg(input())
+    result = get_roots(a, b, c)
     print(result)
 
 main()
